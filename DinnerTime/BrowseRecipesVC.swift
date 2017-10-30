f9b427103c5adf52d1370344691046c266dfbfaf
Caroline Davis <caroline_davis@live.com>
2017-10-30 18:26:39 +1100

    feat: Start of firebase database, need to fix hearts in ui

--- DinnerTime/BrowseRecipesVC.swift
+++ DinnerTime/BrowseRecipesVC.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 import Alamofire
+import FirebaseDatabase
 
 class BrowseRecipesVC: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var usersBrowserField: UITextField!
-    
-
-    
     @IBOutlet weak var contentView: UIView!
-    var stringOfWords: String!
     
+    var stringOfWords: String!
     var tableViewData = [[String: Any]]()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        watchRecipes()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,27 @@ class BrowseRecipesVC: UIViewController, UITextFieldDelegate, UITableViewDelegat
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        ref = Database.database().reference()
     
+    }
+    
+    func watchRecipes() {
+        var userId = "123"
+        
+        ref.child("recipes").child(userId).observe(DataEventType.childAdded, with: { (snapshot) in
+            let recipe = snapshot.value as? [String : AnyObject] ?? [:]
+            var recipeId = recipe["recipe_id"] as! String
+            for (index, dictionary) in self.tableViewData.enumerated() {
+                var currentRecipe = dictionary
+                if currentRecipe["recipe_id"] as! String == recipeId {
+                    currentRecipe["saved"] = true
+                    self.tableViewData[index] = currentRecipe
+                    self.tableView.reloadData()
+                }
+            }
+        })
+        
     }
     
  
@@ -55,6 +78,15 @@ class BrowseRecipesVC: UIViewController, UITextFieldDelegate, UITableViewDelegat
         cell?.viewBtn.tag = indexPath.row
         cell?.viewBtn.addTarget(self, action: #selector(viewRecipeClicked), for: .allEvents)
         
+        // if dictionary["saved"] is true, change the color of the heart
+        let saved = dictionary["saved"] as! Bool
+        let id = dictionary["recipe_id"] as! String
+
+        if saved == true {
+            cell?.heart.setTitleColor(UIColor(red:0.50, green:0.00, blue:0.25, alpha:1.0), for: .normal)
+        }
+        cell?.heart.tag = indexPath.row
+        
         return cell!
     }
     
@@ -70,14 +102,23 @@ class BrowseRecipesVC: UIViewController, UITextFieldDelegate, UITableViewDelegat
     
     // saves the recipe for the recipe book in saved recipes vc
     @IBAction func clickHeart(_ sender: UIButton) {
-    
-        if sender.isSelected == false {
-            sender.setTitleColor(UIColor(red:0.50, green:0.00, blue:0.25, alpha:1.0), for: .normal)
-            sender.isSelected = true
-        } else {
-            sender.setTitleColor(UIColor(red:0.90, green:0.90, blue:0.90, alpha:1.0), for: .normal)
-            sender.isSelected = false
-        }
+    print(sender.tag)
+        let dictionary = self.tableViewData[sender.tag]
+        let website = dictionary["source_url"] as! String
+        let title = dictionary["title"] as! String
+        let recipeId = dictionary["recipe_id"] as! String
+        var userId = "123"
+        
+        ref.child("recipes").child(userId).child(recipeId).setValue(dictionary)
+//        if sender.isSelected == false {
+//            sender.setTitleColor(UIColor(red:0.50, green:0.00, blue:0.25, alpha:1.0), for: .normal)
+//            sender.isSelected = true
+//            // add this recipe to the saved recipes list for the database
+//        } else {
+//            sender.setTitleColor(UIColor(red:0.90, green:0.90, blue:0.90, alpha:1.0), for: .normal)
+//            sender.isSelected = false
+//            // delete this recipe off the saved recipes list for the database
+//        }
         
     }
     
@@ -97,20 +138,49 @@ class BrowseRecipesVC: UIViewController, UITextFieldDelegate, UITableViewDelegat
     }
     
     func searchRecipes() {
+        
         // make the request to the API - remember to add the ! to the stringofwords so its not optional and works
         print("\(API_ADDRESS)\(SEARCH_RECIPES)\(API_KEY)&q=\(stringOfWords)")
         Alamofire.request("\(API_ADDRESS)\(SEARCH_RECIPES)\(API_KEY)&q=\(stringOfWords!)").responseJSON { response in
             
+            // clear tableViewData
+            self.tableViewData.removeAll()
+            
             if let value = response.result.value as? [String: Any] {
                 if let dictionaries = value["recipes"] as? [[String: Any]] {
                     if dictionaries.count > 0 {
-                        self.tableViewData = dictionaries
-                        self.tableView.reloadData()
+                        
+                        // iterate over array of dictionaries... in db?
+                        for dictionary in dictionaries {
+                            var recipe = dictionary
+                            self.searchCurrentSavedRecipes(recipeId: dictionary["recipe_id"] as! String, completion: { result in
+                                
+                                recipe["saved"] = result
+                                self.tableViewData.append(recipe)
+                                self.tableView.reloadData()
+                            
+                            })
+                        }
+                        
                     }
                 }
-                
+            }
+        }
+    }
+    
+    func searchCurrentSavedRecipes(recipeId: String, completion: @escaping (_ hasSaved: Bool) -> Void) {
+        var userId = "123"
+        ref.child("recipes").child(userId).child(recipeId).observeSingleEvent(of: .value, with: { (snapshot) in
+
+            let value = snapshot.value as? NSDictionary
+            if value != nil {
+                completion(true)
+            } else {
+                completion(false)
             }
             
+        }) { (error) in
+            print(error.localizedDescription)
         }
     }
     
--- DinnerTime/Constants.swift
+++ DinnerTime/Constants.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import FirebaseDatabase
 
 let KEY_UID = "uid"
 let API_KEY = "7bbe6ecb98ca9c0e9ec589c0ea4123e1"
@@ -15,3 +16,5 @@ let API_KEY = "7bbe6ecb98ca9c0e9ec589c0ea4123e1"
 let API_ADDRESS = "https://food2fork.com/api"
 let SEARCH_RECIPES = "/search?key="
 let GET_RECIPES = "/get"
+
+var ref: DatabaseReference!
