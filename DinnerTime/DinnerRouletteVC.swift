441b42efe4a43498c746144cfb23905ce2ed6cda
Caroline Davis <caroline_davis@live.com>
2017-11-09 18:26:46 +1100

    feat: Add view btn code for dinner roulette vc

--- DinnerTime/DinnerRouletteVC.swift
+++ DinnerTime/DinnerRouletteVC.swift
@@ -18,6 +18,7 @@ class DinnerRouletteVC: UIViewController {
     @IBOutlet weak var resultsView: UIView!
     
     var recipes: [[String: AnyObject]]!
+    var randomRecipe: Int!
     var angle: CGFloat = 0
     
     override func viewDidLoad() {
@@ -29,51 +30,63 @@ class DinnerRouletteVC: UIViewController {
     
     @IBAction func randomRecipeGenerator() {
         
+        spinPizza()
+        
+        // makes the code below begin after 2 seconds aka. after the pizza has spun!!!!
+        let when = DispatchTime.now() + 1.8
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            
+            let userId = "123"
+            ref.child("recipes").child(userId).observeSingleEvent(of: .value, with: { (snapshot) in
+                let dictionary = snapshot.value as? [String : [String: AnyObject]] ?? [:]
+                
+                // remove the keys, we just want the values
+                let values = Array(dictionary.values) as [[String: AnyObject]]
+                self.recipes = values
+                
+                // this will give us the number of the random index it has chosen
+                self.randomRecipe = Int(arc4random_uniform(UInt32(self.recipes.count)))
+                // this will select the chosen index in the recipes array
+                let recipe = self.recipes[self.randomRecipe]
+                
+                // need to extract the title of the recipe + check if it is empty
+                let recipeName = recipe["title"]
+                if recipeName != nil {
+                    self.recipeLabel.text = (recipeName?.capitalized)! as String
+                    self.resultsView.isHidden = false
+                } else {
+                    self.recipeLabel.text  = "No Name Recipe"
+                    self.resultsView.isHidden = false
+                }
+                
+            })
+            
+        }
+    }
+    
+    func spinPizza() {
+        
+        // gets the angle to change when the button is clicked to continually do 360 spins
         var newAngle: CGFloat = angle + 180
         angle = newAngle
         
-        print("CAROL THIS IS NEW ANGLE\(newAngle)")
-        print("carol this is angle \(angle)")
-    
         // rotates the uiview
         UIView.animate(withDuration: 2.0, animations: {
             self.pizza.transform = CGAffineTransform(rotationAngle: (newAngle * CGFloat(M_PI)) / 180)
         })
 
-        
-        // makes the code below begin after 2 seconds aka. after the pizza has spun!!!!
-        let when = DispatchTime.now() + 1.8
-        DispatchQueue.main.asyncAfter(deadline: when) {
-        
-        let userId = "123"
-        ref.child("recipes").child(userId).observeSingleEvent(of: .value, with: { (snapshot) in
-            let dictionary = snapshot.value as? [String : [String: AnyObject]] ?? [:]
-            
-            // remove the keys, we just want the values
-            let values = Array(dictionary.values) as [[String: AnyObject]]
-
-            self.recipes = values
-            
-            // this will give us the number of the random index is has chosen
-            let randomRecipe = Int(arc4random_uniform(UInt32(self.recipes.count)))
-            
-            // this will select the chosen index in the recipes array
-            let recipe = self.recipes[randomRecipe]
-            
-            // need to extract the title of the recipe + check if it is empty
-            let recipeName = recipe["title"]
-            if recipeName != nil {
-                self.recipeLabel.text = (recipeName?.capitalized)! as String
-                self.resultsView.isHidden = false
-            } else {
-                self.recipeLabel.text  = "No Name Recipe"
-                self.resultsView.isHidden = false
-            }
-            
-        })
-        
-        }
     }
+    
+    @IBAction func openViewButton(sender: UIButton) {
+        
+        // gets the randomIndex chose from arc4random and gets the URL... opens it in safari
+        let recipe = self.recipes[self.randomRecipe]
+        let recipeURL = recipe["source_url"] as! String
+        UIApplication.shared.open(URL(string: recipeURL)!, options: [:], completionHandler: nil)
+        
+    }
+
+
 
 
 }
